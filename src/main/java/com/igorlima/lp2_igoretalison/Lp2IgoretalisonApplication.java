be0cc5d29cc777221d5e2cdba67b9116287e7dfc
Igor Pereira de Lima <igo_lima10@hotmail.com>
2021-02-03 14:00:07 -0300

    first commit

---
+++ src/main/java/com/igorlima/lp2_igoretalison/Lp2IgoretalisonApplication.java
@@ -0,0 +1,13 @@
+package com.igorlima.lp2_igoretalison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2IgoretalisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2IgoretalisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorlima/lp2_igoretalison/Lp2IgoretalisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorlima.lp2_igoretalison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2IgoretalisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
